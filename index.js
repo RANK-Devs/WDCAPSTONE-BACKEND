7b7610ebb6f741b345101435cad39831f28aa0fd
UK-devs <156414810+UK-devs@users.noreply.github.com>
2024-03-11 00:20:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
     origin: 'https://wdcapstone-frontend1.onrender.com',
-    credentials: true
+    //credentials: true
 }));
 
 app.use("/api/auth", authRoute);
