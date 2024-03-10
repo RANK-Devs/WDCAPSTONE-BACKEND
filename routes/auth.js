5640c3d4cb74d06aa1f33af11ead8d5722f2ec15
UK-devs <156414810+UK-devs@users.noreply.github.com>
2024-03-11 00:46:32 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import { login, register, resetPassword, sendEmail } from "../controllers/auth.controller.js";
 
 const router = express.Router();
